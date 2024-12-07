ce1f7f7bd576da30b43a553e08846e8f53f2b7f8
ChesterEGG <56885368+ChesterEGG@users.noreply.github.com>
2024-12-07 23:12:11 +0100

    Algorithme de Floyd-Warshall

--- algo/Dijkstra.py
+++ algo/Dijkstra.py
@@ -64,19 +64,3 @@ def Dijkstra(C):
         D[i] = chemins_plus_court(i)
 
     return D
-
-
-C = np.array([
-    # A    B    C    D    E    F    G    H    I    J
-    [0,   4,   4,   3,   INF, INF, INF, INF, INF, INF],  # A
-    [4,   0,   1,   INF, 3,   INF, 4,   INF, INF, INF],  # B
-    [4,   1,   0,   3,   3,   INF, INF, INF, INF, INF],  # C
-    [3,   INF, 3,   0,   INF, 4,   INF, INF, 5,   INF],  # D
-    [INF, 3,   3,   INF, 0,   5,   5,   3,   INF, INF],  # E
-    [INF, INF, INF, 4,   5,   0,   INF, 4,   4,   5],    # F
-    [INF, 4,   INF, INF, 5,   INF, 0,   3,   INF, INF],  # G
-    [INF, INF, INF, INF, 3,   4,   3,   0,   INF, 5],    # H
-    [INF, INF, INF, 5,   INF, 4,   INF, INF, 0,   5],    # I
-    [INF, INF, INF, INF, INF, 5,   INF, 5,   5,   0]     # J
-])
-print(Dijkstra(C))
--- algo/Floyd_Warshall.py
+++ algo/Floyd_Warshall.py
@@ -1,1 +1,28 @@
-# TODO
+import numpy as np
+
+# Initiation des variables
+INF = float('inf')
+
+
+def Floyd_Warshall(C):
+    """
+        Calcule la matrice de distance des plus courts chemins entre toutes les paires de noeuds d’un graphe en utilisant
+        l'algorithme de Floyd-Warshall.
+
+        input: C --> une matrice numpy n x n de coût C
+        output: Une matrice n × n contenant les distances des plus courts chemins
+                entre toutes les paires de noeuds d'un graphe.
+    """
+    # Nombre de noeuds
+    n = len(C)
+    # Copier la matrice coût sur D
+    D = C.copy()
+
+    for i in range(n):  # Sommet intermédiaire
+        for j in range(n):  # Sommet source
+            for k in range(n):  # Sommet destination
+                # Misa à jour de la distance minimale
+                if D[i][k] != INF and D[k][j] != INF:  # Eviter les additions avec des INF
+                    D[i][j] = min(D[i][j], D[i][k] + D[k][j])
+
+    return D
